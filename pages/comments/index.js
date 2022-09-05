41e9c282a89715ee380aba2d7efd49f86d03c531
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-09-05 11:26:21 +0600

    delete data from local api

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -22,7 +22,16 @@ const CommentsPage = () => {
     })
     const data = await response.json();
     console.log(data);
-}
+    }
+
+    const deleteComment = async (commentId)=>{
+        const response = await fetch(`/api/comments/${commentId}`, {
+            method: 'DELETE',
+        })
+        const data = await response.json();
+        console.log(data);
+        fetchComments();
+    }
 
     return (
         <>
@@ -36,7 +45,9 @@ const CommentsPage = () => {
             comments.map(comment =>{
                 return(
                     <div key={comment.id}>
-                        <h2>{comment.text}</h2>
+                        <h2>{comment.id}{comment.text}</h2>
+
+                        <button onClick={()=>deleteComment(comment.id)}>Delete</button>
                     </div>
                 )
             })
